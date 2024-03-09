061dc938b9e8919cd7a35a854be0eedba4346995
Anthony <c0033688@newcaste.ac.uk>
2024-03-09 17:19:24 +0000

    init commit

---
+++ reveal-animation.js
@@ -0,0 +1,22 @@
+function handleScroll() {
+    var reveals = document.querySelectorAll(".reveal");
+
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 5;
+
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+
+document.addEventListener("scroll", function() {
+    setTimeout(() => {
+        handleScroll();
+    }, 100)
+});
