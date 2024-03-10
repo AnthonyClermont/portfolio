b1ad6edd37177f7b9c5fef74669ea543bb8aed7e
Anthony <c0033688@newcaste.ac.uk>
2024-03-10 15:51:29 +0000

    relase 0.1

---
+++ dist/index.0a004941.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("person"),o=document.getElementById("window"),t=document.getElementById("desk");window.addEventListener("scroll",()=>{let n=window.scrollY;e.style.marginTop=1*n+"px",e.style.opacity=100-n/4+"%",o.style.opacity=100-n/2+"%",t.style.marginTop=.5*n+"px",t.style.opacity=100-n/8+"%"}),window.onload=function(){window.location.hash&&(window.location.hash="")};
+//# sourceMappingURL=index.0a004941.js.map
